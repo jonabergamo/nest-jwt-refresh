7e5b7443b2353c5292e664b24422d333f8b3c7c4
jonabergamo <jonathanbergamo16@gmail.com>
2024-03-14 17:36:23 -0300

    feat: first commit

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_AT_SECRET,
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    return payload;
+  }
+}
